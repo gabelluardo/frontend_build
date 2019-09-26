da048a026de681827455e364485dbc35786ef142
Gabriele <gabriele.belluardo@outlook.it>
2019-09-26 11:53:46 +0200

    v0.6.0

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bfddfd48481e9942741e912efdc08723.js"
+  "/precache-manifest.995802492d8d9bd21522d1ef62c39a15.js"
 );
 
 self.addEventListener('message', (event) => {
